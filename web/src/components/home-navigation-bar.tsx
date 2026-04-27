bfd465885519c7209d4c7029cac0ccdff763140f
Alex Rodriguez <rodriguez.al183@gmail.com>
2026-04-27 12:52:05 -0700

    web(home-navbar): Add initial home page navigation bar

---
+++ web/src/components/home-navigation-bar.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import type React from 'react';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '#/components/ui/navigation-menu';
+
+const components: {
+  id: string;
+  title: string;
+  href: string;
+  description: string;
+}[] = [
+  {
+    id: 'about',
+    title: 'About Sync Pad',
+    href: '/about',
+    description: 'Learn more about what we have to offer',
+  },
+  {
+    id: 'docs',
+    title: 'Documentation',
+    href: '/docs',
+    description: 'Get started with Sync Pad',
+  },
+];
+
+export default function HomeNavigationMenu() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting Started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="flex flex-col items-start gap-2 min-w-86 max-w-112">
+              {components.map((item) => (
+                <ListItem
+                  key={item.id}
+                  href={item.href}
+                  title={item.title}
+                  className="w-full"
+                >
+                  {item.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+            <a href="/docs">Docs</a>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<'li'> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <a href={href} className="block w-full text-left">
+          <div className="flex flex-col items-start gap-1 text-left text-sm">
+            <div className="leading-none font-medium">{title}</div>
+            <div className="line-clamp-2 text-muted-foreground">{children}</div>
+          </div>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+}
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
+import HomeNavigationMenu from '#/components/home-navigation-bar';
 
 export const Route = createFileRoute('/')({ component: Home });
 
 function Home() {
   return (
     <div className="p-8">
+      <HomeNavigationMenu />
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
