6b3ef9d5bba1c5e1ecfb2b0eda04161472af2edb
Alex Rodriguez <rodriguez.al183@gmail.com>
2026-04-27 21:37:23 -0700

    fix(api): remove old health_check table from migrations

--- api/drizzle/0000_rapid_rocket_raccoon.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "health_checks" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"service" text NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL
-);
