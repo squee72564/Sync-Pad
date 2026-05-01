dbdf78d7f9439442418b6d405cc007174ffb2a61
Alex Rodriguez <rodriguez.al183@gmail.com>
2026-05-01 12:21:56 -0700

    fix(packages/db): fix tsconfig and drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 
+if (!process.env.DATABASE_URL) {
+  console.error(`DATABASE_URL required, found: ${process.env.DATABASE_URL}`);
+  process.exit(1);
+}
+
 export default defineConfig({
   schema: './src/schema/*',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
